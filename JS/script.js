71ab9cbfb36052dacddcb3cefa8f35db41348699
TONNYCHWEYA <chweyatonny@gmail.com>
2022-05-16 10:06:25 +0300

    create variables

--- JS/script.js
+++ JS/script.js
@@ -1,5 +1,6 @@
+ function onSubmit(){
  //array of answers
- var Answer =[
+ var answer =[
    "Object-Based",
    "block that combines a number of statements into a single compound statement",
    "Ignores the statements",
@@ -8,4 +9,26 @@
 
  ];
  var score =0;
- var 
+ var noOfQuestions =5;
+ var q1 = document.forms["form"]["Q1"].value;
+ var q2 =document.forms["form"]["Q2"].value;
+ var q3= document.forms["form"]["Q3"].value;
+ var q4 = document.forms["form"]["Q4"].value;
+ var q5 = document.forms["form"]["Q5"].value;
+
+ //compare the answers
+ //if answer was not selected
+ for(var i = 1; i <= noOfQuestions; i++){
+   if('q' + i == ''){
+     alert("You missed question" i);
+   }
+
+ }
+ for(var i = 1; i <= noOfQuestions; i++){
+  if('q' + i == answer(i -1)){
+    score++;
+  }
+
+
+}
+ }

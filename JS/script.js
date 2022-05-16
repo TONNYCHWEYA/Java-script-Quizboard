3e2fda90829c5c2a060aa884c65774a93b8eeb66
TONNYCHWEYA <chweyatonny@gmail.com>
2022-05-16 09:12:32 +0300

    created array of anwsers

---
+++ JS/script.js
@@ -0,0 +1,11 @@
+ //array of answers
+ var Answer =[
+   "Object-Based",
+   "block that combines a number of statements into a single compound statement",
+   "Ignores the statements",
+   "Declaration statements",
+   "The local element"
+
+ ];
+ var score =0;
+ var 

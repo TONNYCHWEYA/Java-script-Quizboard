c266da6de7be41ac94bee079fdb62ce1d9f95b1d
TONNYCHWEYA <chweyatonny@gmail.com>
2022-05-17 20:10:10 +0300

    display function created

--- JS/script.js
+++ JS/script.js
@@ -1,11 +1,11 @@
  function onSubmit(){
  //array of answers
  var answer =[
-   "Object-Based",
-   "block that combines a number of statements into a single compound statement",
-   "Ignores the statements",
-   "Declaration statements",
-   "The local element"
+   'Object-Based',
+   'block that combines a number of statements into a single compound statement',
+   'Ignores the statements',
+   'Declaration statements',
+   'The local element'
 
  ];
  var score =0;
@@ -15,20 +15,38 @@
  var q3= document.forms["form"]["Q3"].value;
  var q4 = document.forms["form"]["Q4"].value;
  var q5 = document.forms["form"]["Q5"].value;
-
+ var results = document.getElementById('results');
  //compare the answers
  //if answer was not selected
- for(var i = 1; i <= noOfQuestions; i++){
-   if('q' + i == ''){
-     alert("You missed question" i);
+ /*for(var i = 1; i <= noOfQuestions; i++){
+   if(eval('q' + i) == ''){
+     alert("You missed question" + i);
    }
+   
 
  }
+ */
  for(var i = 1; i <= noOfQuestions; i++){
-  if('q' + i == answer(i -1)){
+  if(eval('q' + i) == answer[i -1]){
     score++;
   }
-
-
-}
  }
+ 
+ 
+ document.getElementById("results").innerText = scoreDiplay(score);
+ alert(score);
+  
+
+ }
+ function scoreDiplay(score) {
+  var score = (score / 5) * 100;
+  var message = "";
+  if (score < 50) {
+    message = "Poor score retake the quiz";
+  } else if (score < 80) {
+    message = "Average score keep it up!";
+  } else {
+    message = "Excellent!";
+  }
+  return score + "%: " + message;
+}
